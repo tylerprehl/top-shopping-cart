dd0512d74f9e1cce5cc26f4220c5e9e6fa587c18
Tyler Prehl <tyler@prehl.us>
2024-01-27 10:26:23 -0500

    Re-organize Home page and Store CSS, add store name and witty comment to home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,14 @@
 import './Home.css';
 
 function Home() {
-
   return (
     <>
-      <h1>Home</h1>
-      <p className="home-container">
-          Welcome to the Home page
-      </p>
+      <h1 className="store-name">
+        <strong>Tyler's Tech Parts</strong>
+      </h1>
+      <p className="intro-statement">The store for all your tech needs! Hard drives? Monitors? The robots from iRobot? We'll see what we have out back!</p>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
